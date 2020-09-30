4699144317a7dd9c9af3e95761e43444fe0592a4
Rachel Peterson <69697727+rachelpeterson89@users.noreply.github.com>
2020-09-30 15:44:54 -0500

    Create thunderdome.py

---
+++ thunderdome.py
@@ -0,0 +1,12 @@
+import groupComp
+
+brody = groupComp.Fighter("Brody",60,10,25)
+josh = groupComp.Fighter("Josh",10,40,40)
+ruben = groupComp.Fighter("Ruben",40,20,50)
+rachel = groupComp.Fighter("Rachel",40,50,70)
+
+et = groupComp.Alien()
+adrien = groupComp.Robot()
+cowboy = groupComp.Cowboy()
+
+groupComp.fight(adrien,cowboy)
